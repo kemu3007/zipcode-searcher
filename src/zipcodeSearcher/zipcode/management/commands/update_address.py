44ccd6e64f21a1420af755299643f2535d9ecbcc
EC2 Default User <ec2-user@ip-172-31-39-216.ap-northeast-1.compute.internal>
2020-01-31 15:44:54 +0000

    コマンドの追加、登録

--- src/zipcodeSearcher/zipcode/management/commands/update_address.py
+++ src/zipcodeSearcher/zipcode/management/commands/update_address.py
@@ -1,18 +1,28 @@
+import pandas as pd
+
 from django.core.management.base import BaseCommand
 
+from zipcodeSearcher.settings import BASE_DIR
 from zipcodeSearcher.zipcode.models import Address
 
 
 # BaseCommandを継承して作成
 class Command(BaseCommand):
     # python manage.py help count_entryで表示されるメッセージ
-    help = 'Display the number of blog articles'
+    help = 'reload address data from csv'
 
-    # コマンドライン引数を指定します。(argparseモジュール https://docs.python.org/2.7/library/argparse.html)
-    # 今回はblog_idという名前で取得する。（引数は最低でも1個, int型）
-    def add_arguments(self, parser):
-        pass
-
-    # コマンドが実行された際に呼ばれるメソッド
     def handle(self, *args, **options):
-        pass
+        df = pd.read_csv(f'{BASE_DIR}/address_data/KEN_ALL.CSV', encoding='SHIFT_JIS')
+        for index, row in df.iterrows():
+            Address.objects.get_or_create(
+                code = row[0],
+                old_zipcode = row[1],
+                zipcode = row[2],
+                prefecture_kana = row[3],
+                city_kana = row[4],
+                town_area_kana = row[5],
+                prefecture = row[6],
+                city = row[7],
+                town_area = row[8]
+            )
+            print(f'\r{index}/{len(df)}', end='')
