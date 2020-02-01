26207e49283037abc95e4bf3ada10e1d496c0a97
EC2 Default User <ec2-user@ip-172-31-39-216.ap-northeast-1.compute.internal>
2020-02-01 01:19:28 +0000

    検索を可能に

--- src/zipcodeSearcher/zipcode/views.py
+++ src/zipcodeSearcher/zipcode/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from zipcodeSearcher.zipcode.serializer import AddressSerializer
+from zipcodeSearcher.zipcode.models import Address
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        zipcode = self.request.query_params.get('zipcode', None)
+        if zipcode:
+            zipcode = zipcode.replace('-', '')
+            queryset = Address.objects.filter(zipcode=zipcode)
+        else:
+            pass
+        return queryset
